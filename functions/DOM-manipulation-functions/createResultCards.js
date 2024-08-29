6b5389026257c14d03d8efab8c9b71f57c5495d3
Tyler Huyser <tyler.huyser@gmail.com>
2024-08-28 21:04:16 -0400

    re-activated FOR loop for debugging.

--- functions/DOM-manipulation-functions/createResultCards.js
+++ functions/DOM-manipulation-functions/createResultCards.js
@@ -5,33 +5,33 @@ export default function createResultCards(resultsContainerID, data, resultsSecti
 
   console.log(data)
   
-  // for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
 
-  //   let id = data[i].id
-  //   let name = data[i].consensus.name
-  //   let imageID = data[i].primary_image.id
+    let id = data[i].id
+    let name = data[i].consensus.name
+    let imageID = data[i].primary_image.id
 
-  //   const result = document.createElement('div')
-  //   result.classList.add('result-card')
-  //   result.id = `result ${id}`
-  //   document.getElementById(resultsContainerID).appendChild(result)
-  //   // ^Above: Creates a div object to contain each result
+    const result = document.createElement('div')
+    result.classList.add('result-card')
+    result.id = `result ${id}`
+    document.getElementById(resultsContainerID).appendChild(result)
+    // ^Above: Creates a div object to contain each result
   
-  //   const cardTitle = document.createElement('a');
-  //   let linkText = document.createTextNode(`${name}`);
-  //   cardTitle.appendChild(linkText);
-  //   cardTitle.title = `${name}`
-  //   let URL = `https://mushroomobserver.org/${id}`
-  //   cardTitle.href = `${URL}`
-  //   result.appendChild(cardTitle)
-  //   // ^Above: Creates an 'a' hyperlink element containing Mushroom Name and linking to the Observation Page on Mushroom World using the observation ID.
+    const cardTitle = document.createElement('a');
+    let linkText = document.createTextNode(`${name}`);
+    cardTitle.appendChild(linkText);
+    cardTitle.title = `${name}`
+    let URL = `https://mushroomobserver.org/${id}`
+    cardTitle.href = `${URL}`
+    result.appendChild(cardTitle)
+    // ^Above: Creates an 'a' hyperlink element containing Mushroom Name and linking to the Observation Page on Mushroom World using the observation ID.
   
-  //   const cardImage = document.createElement('img')
-  //   let imageURL = `https://images.mushroomobserver.org/320/${imageID}.jpg`
-  //   cardImage.src = imageURL
-  //   result.appendChild(cardImage)
+    const cardImage = document.createElement('img')
+    let imageURL = `https://images.mushroomobserver.org/320/${imageID}.jpg`
+    cardImage.src = imageURL
+    result.appendChild(cardImage)
 
-  // }
+  }
 
     removeResults(resultsSection)
 
